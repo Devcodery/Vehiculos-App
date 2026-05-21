8cc6e70ba42ac94874fec82d58d429ede7098515
devcodery <devcoderys@gmail.com>
2026-05-21 06:38:13 +0200

    feat: mejorar el manejo de usuario y token en el store de autenticación

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,10 +1,10 @@
-// src/stores/auth.js
 import { defineStore } from 'pinia'
 import api from '@/services/api'
+import { ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    user: null,
+    user: JSON.parse(localStorage.getItem('user')) || null,
     token: localStorage.getItem('token') || null,
   }),
   getters: {
@@ -20,7 +20,10 @@ export const useAuthStore = defineStore('auth', {
 
         const response = await api.post('/token', formData)
         this.token = response.data.access_token
+        this.user = response.data.user_data
+
         localStorage.setItem('token', this.token)
+        localStorage.setItem('user', JSON.stringify(this.user))
         
         return { success: true }
       } catch (error) {
@@ -32,6 +35,7 @@ export const useAuthStore = defineStore('auth', {
       this.token = null
       this.user = null
       localStorage.removeItem('token')
+      localStorage.removeItem('user')
     }
   }
 })
